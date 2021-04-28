30963a4012eb1b66a04a3abdb08a0886cb9a5738
danicharles99 <danicharles1999@gmail.com>
2021-04-28 18:30:25 +0200

    Collect pickup's

--- Lab1-RollaBall/Assets/Scripts/PlayerController.cs
+++ Lab1-RollaBall/Assets/Scripts/PlayerController.cs
@@ -32,4 +32,12 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movement * speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
