11a9a0dac76b7bf09206d1e2e84ffaed1dd89aa8
danicharles99 <danicharles1999@gmail.com>
2021-04-28 19:01:58 +0200

    Add win msg

--- Lab1-RollaBall/Assets/Scripts/PlayerController.cs
+++ Lab1-RollaBall/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
     public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
     private int count;
@@ -22,6 +23,7 @@ public class PlayerController : MonoBehaviour
         count = 0;
 
         SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -35,6 +37,11 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
+
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     void FixedUpdate()
