7a4ae6416f2c1c1cdd42ea8bc2e80e3b268565f7
danicharles99 <danicharles1999@gmail.com>
2021-05-01 13:15:52 +0200

    Move components of the Hay Bale Model to parent + add sheep

---
+++ Lab2-Scripting/Assets/RW/Scripts/Sheep.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    public float runSpeed; 
+    public float gotHayDestroyDelay; 
+    private bool hitByHay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+    }
+
+    private void HitByHay()
+    {
+        hitByHay = true; 
+        runSpeed = 0; 
+
+        Destroy(gameObject, gotHayDestroyDelay);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Hay") && !hitByHay) 
+        {
+            Destroy(other.gameObject); 
+            HitByHay(); 
+        }
+    }
+}
