7fe29386da41393a91bc05c897dbec25a5a2e5a8
danicharles99 <danicharles1999@gmail.com>
2021-05-01 12:18:29 +0200

    Add haymachine boundaries

--- Lab2-Scripting/Assets/RW/Scripts/HayMachine.cs
+++ Lab2-Scripting/Assets/RW/Scripts/HayMachine.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HayMachine : MonoBehaviour
 {
     public float movementSpeed;
+    public float horizontalBoundary = 22;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +18,25 @@ public class HayMachine : MonoBehaviour
     void Update()
     {
         UpdateMovement();
-
     }
 
     private void UpdateMovement()
     {
         float hInputValue = Input.GetAxisRaw("Horizontal"); // Values {-1, 0, 1}
-        transform.Translate(transform.right * hInputValue * movementSpeed * Time.deltaTime);
+
+        
+        Vector3 translateValue = transform.right * hInputValue * movementSpeed * Time.deltaTime;
+        Vector3 targetPos = transform.position + translateValue;
+
+        if (PositionInsideBoundaries(targetPos))
+        {
+            transform.Translate(translateValue);
+        }
+    }
+
+    private bool PositionInsideBoundaries(Vector3 position)
+    {
+        return position.x > -horizontalBoundary && position.x < horizontalBoundary;
+
     }
 }
