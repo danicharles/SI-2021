f75b1d313ff4bd8317edcf40eb5711de2019ba7f
danicharles99 <danicharles1999@gmail.com>
2021-05-01 12:05:21 +0200

    Add movment to the hatMachine

---
+++ Lab2-Scripting/Assets/RW/Scripts/HayMachine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HayMachine : MonoBehaviour
+{
+    public float movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+
+    }
+
+    private void UpdateMovement()
+    {
+        float hInputValue = Input.GetAxisRaw("Horizontal"); // Values {-1, 0, 1}
+        transform.Translate(transform.right * hInputValue * movementSpeed * Time.deltaTime);
+    }
+}
